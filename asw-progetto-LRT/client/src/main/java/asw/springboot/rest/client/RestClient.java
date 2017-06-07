d48ee21453712662fc55af6a34e30cd14573f7ac
Carlo Loffredo <carlettof@yahoo.it>
2017-06-07 12:44:06 +0200

    URIs specified as properties

--- asw-progetto-LRT/client/src/main/java/asw/springboot/rest/client/RestClient.java
+++ asw-progetto-LRT/client/src/main/java/asw/springboot/rest/client/RestClient.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.util.*;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,12 +14,15 @@ import org.springframework.web.client.RestTemplate;
 public class RestClient implements CommandLineRunner {
     
     private static final Logger logger = LoggerFactory.getLogger(RestClient.class);
+
+    @Value("${word.s2.uri}")
+    private String s2URI;
     
     public void run(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
         
         //		String artistaQueen = restTemplate.getForObject("http://localhost:8080/S/Queen", String.class);
-        String artistaQueen = restTemplate.getForObject("http://localhost:8081/S1/{name}", String.class, "Queen");
+        String artistaQueen = restTemplate.getForObject(s2URI + "Queen", String.class);
         
         /* ripete tante volte, altrimenti si perde tra i log di Spring Boot */
         for (int i=0; i<3; i++) {
--- asw-progetto-LRT/s-service/src/main/java/asw/springboot/rest/s/SController.java
+++ asw-progetto-LRT/s-service/src/main/java/asw/springboot/rest/s/SController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable; 
 import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
 
@@ -14,13 +15,17 @@ import javax.annotation.PostConstruct;
 public class SController {
 	
 	private RestTemplate restTemplate = new RestTemplate();
-   
+   	
+	@Value("${word.s1.uri}")
+	private String s1URI;
+	@Value("${word.s2.uri}")
+	private String s2URI;
 	
 	/* Restituisce info generiche sull'{artista} specificato */
 	@RequestMapping("/S/{artista}")
 	public String artistaS(@PathVariable String artista) {
-		String info = restTemplate.getForObject("http://localhost:8081/S1/{artista}", String.class, artista);
-		String infoAlbum = restTemplate.getForObject("http://localhost:8082/S2/{artista}", String.class, artista);
+		String info = restTemplate.getForObject(s1URI + artista, String.class);
+		String infoAlbum = restTemplate.getForObject(s2URI + artista, String.class);
 
 		if(!(info.equals("fail")))
 			return "I " + artista + " hanno " + info + " e hanno " + infoAlbum ;
@@ -29,8 +34,8 @@ public class SController {
 	
 	@RequestMapping("/S/{artista}/{album}")
 	public String artistaSalbum(@PathVariable String artista, @PathVariable String album) {
-		String info = restTemplate.getForObject("http://localhost:8081/S1/{artista}", String.class, artista);
-		String infoAlbum = restTemplate.getForObject("http://localhost:8082/S2/{artista}/{album}", String.class, artista, album);
+		String info = restTemplate.getForObject(s1URI + artista, String.class);
+		String infoAlbum = restTemplate.getForObject(s2URI + artista + "/" + album, String.class);
 
 		if(info.equals("fail"))
 			return "I " + artista + " non sono presenti nel database";
